25d22805b24fe49fa673e163eaf79f6d3f29e7f0
William <hey_its_william_k@hotmail.com>
2017-04-17 13:17:39 +1000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,18 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+  Todo
+  .find()
+  .then(
+    (todos) => {
+      res.send({todos})
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  )
+})
 
 
 
